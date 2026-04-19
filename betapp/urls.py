bfa921ff0eef1996eae65e313ff1927213ee16cb
manrays354 <161753078+manrays354@users.noreply.github.com>
2026-04-19 15:44:53 +0300

    Update urls.py

--- betapp/urls.py
+++ betapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.get_games_api, name='get_games'), 
-    path('pay/', views.initiate_payment, name='initiate_payment'),
+    path('initiate_payment/', views.initiate_payment, name='initiate_payment'),
     path('payhero_callback/', views.payhero_callback, name='payment_callback'),
 ]
