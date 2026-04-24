9a58c748598f6fdc0b24220b9623161707af5306
manrays354 <161753078+manrays354@users.noreply.github.com>
2026-04-24 10:37:58 +0300

    Update urls.py

--- betapp/urls.py
+++ betapp/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from .views import BotViewSet 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 
+router = DefaultRouter()
+router.register(r'bot', BotViewSet, basename='bot')
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('', views.get_games_api, name='get_games'), 
     path('initiate_payment/', views.initiate_payment, name='initiate_payment'),
     path('payhero_callback/', views.payhero_callback, name='payment_callback'),
